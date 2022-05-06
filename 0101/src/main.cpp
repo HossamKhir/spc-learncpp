209f1d28d876ea68ab30a1f00e9f49738d20f1ec
Hossam Khair <hossam.khir1@outlook.com>
2022-05-06 23:17:25 +0200

    feat: finished lesson 1 chapter 1

---
+++ 0101/src/main.cpp
@@ -0,0 +1,13 @@
+// preprocessor directive
+#include <iostream> // to use the cout/cin functions to use console
+
+// define a main function
+int main()
+{ // scope starts here
+    // statement
+    std::cout << "Hello, World!"; 
+    // semi-colon is required to tell the termination of the statement, otherwise it is a syntax error
+    
+    // return statement
+    return 0;
+} // score ends here
