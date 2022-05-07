c08111729b3b18468fbad05b3206fad0081a3de1
Hossam Khair <hossam.khir1@outlook.com>
2022-05-07 19:52:00 +0200

    feat: finished lesson 3 chapter 1

---
+++ 0103/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    // declaration statement
+    // data types: integers
+    int x;
+    // doubles
+    double width;
+
+    // defining multiple variables of same type
+    int a, b;
+    return 0;
+}
