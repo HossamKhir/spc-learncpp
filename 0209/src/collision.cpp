1d61d54a1359631aafbcacf3abb43d17a4761a51
Hossam Khair <hossam.khir1@outlook.com>
2022-08-16 17:59:53 +0200

    feat: finished lesson 9 chapter 2

---
+++ 0209/src/collision.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int cout(void)
+{
+    return 5;
+}
+
+int main(void)
+{
+    cout << "Hello, World!";
+    return 0;
+}
---
+++ 0209/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello, World!"; // using `cout` from standard namespace `std`
+    // ^^: the `::` is called scope resolution operator
+    return 0;
+}
